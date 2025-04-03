e425c60ff12397c9c7cc6578ecf9938b6e5c3951
ramezzateff <ramezatef62@gmail.com>
2025-04-03 14:33:06 +0200

    Super

--- 1-Python/Level5/7_compress_to_zip.py
+++ 1-Python/Level5/7_compress_to_zip.py
@@ -1,3 +1,19 @@
 #!/usr/bin/python3
 # Compress a File into a ZIP Archive
 # 	Write a script that compresses a file into a ZIP archive.
+import os
+import zipfile  # for ziping the file
+
+# to create and write
+current_dir = os.path.dirname(os.path.abspath(__file__))
+file_path = os.path.join(current_dir, 'task7.txt')
+
+print(file_path)
+
+with open(file_path, 'a') as file_name:
+    pass
+
+zip_file_name = 'compress_file.zip'
+
+with zipfile.ZipFile(zip_file_name, 'w') as ZipFile:
+    ZipFile.write(zip_file_name, arcname=file_path)
--- 1-Python/Level5/8_find_largest_file.py
+++ 1-Python/Level5/8_find_largest_file.py
@@ -1,3 +1,25 @@
 #!/usr/bin/python3
 # Find the Largest File in a Directory
 # 	Write a script that scans a directory and finds the largest file.
+import os
+
+
+def find_largest_file(current_dir):
+
+    largest_file = ''
+    largest_size = 0
+    for root, dirs, files in os.walk(current_dir):  # walk on all path
+        for file in files:
+            # get path of everyfile and get the size of it
+            file_path = os.path.join(current_dir, file)
+            file_size = os.path.getsize(file_path)
+            if file_size > largest_size:
+                largest_size = file_size
+    largest_file = file_path
+    return largest_file
+
+
+# get the current directory
+current_dir = os.path.dirname(os.path.abspath(__file__))
+
+print(find_largest_file(current_dir))
