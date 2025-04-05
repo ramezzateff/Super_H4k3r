e3819aa96233ad061e1f03476f13be03eaf99f87
ramezzateff <ramezatef62@gmail.com>
2025-04-05 08:50:33 +0200

    Super

---
+++ 1-Python/Level1/4_reverse_string.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+# Reverse the string “rekcah_repus” without using built-in functions.
+x = "rekcah_repus"
+print(x[::-1])
+# Output : super_hacker
--- 2-Network/Day_1/0_validate_ip.py
+++ 2-Network/Day_1/0_validate_ip.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 # Write a Python script that validates
-# 	whether a given IP address is IPv4 or IPv6.
+# whether a given IP address is IPv4 or IPv6.
