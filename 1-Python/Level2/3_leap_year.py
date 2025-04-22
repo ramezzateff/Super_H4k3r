367a52d674a1e876dda757682d53d5a3ecb8db02
ramezzateff <ramezatef62@gmail.com>
2025-04-22 13:31:00 +0200

    Leap year

--- 1-Python/Level2/3_leap_year.py
+++ 1-Python/Level2/3_leap_year.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 # Check if a given year is a leap year.
-# leap year is a year can divisable by 4 and 100 and 400
+# leap year is a year can divisable by 4 o 400 but canot divisable by 100
 leap_year = int(input("put a year to check if it a leap year or not: "))
 if (leap_year % 4 == 0 and leap_year % 100 != 0) or (leap_year % 400 == 0):
     print(f"{leap_year} is a leap year")
