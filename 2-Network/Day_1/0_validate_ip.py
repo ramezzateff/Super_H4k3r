137b228ba7b3271888aa116e4e9bf4934e07eb52
ramezzateff <ramezatef62@gmail.com>
2025-05-21 20:09:20 +0300

    Valid_Ip

--- 2-Network/Day_1/0_validate_ip.py
+++ 2-Network/Day_1/0_validate_ip.py
@@ -4,6 +4,17 @@
 # >>> need to get an input from user and valid ip
 #  a.b.c.d < {ipv4} &ipv6 > x:x:x:x:x:x:x:x
 
-import re
-ip_take_from_user = input('Enter the ip to check if it valid: ')
-print(type(ip_take_from_user))
+# Ref: https://www.geeksforgeeks.org/python-program-to-validate-an-ip-address/
+
+import ipaddress
+
+def check(ip):
+	try:
+		ipaddress.ip_address(ip)
+		print('valid ip address')
+	except ValueError:
+		print("Invalid IP address")
+
+if __name__ == '__main__':
+	ip_take_from_user = input('Enter the ip to check if it valid: ')
+	check(ip_take_from_user)
